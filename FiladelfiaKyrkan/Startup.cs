fa63bb7dd85d71f6744ec242c21d1cb70b2e48d1
bereketbruk <bereketbruk@outlook.com>
2018-09-11 10:42:05 +0200

    Add project files.

---
+++ FiladelfiaKyrkan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FiladelfiaKyrkan.Startup))]
+namespace FiladelfiaKyrkan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
